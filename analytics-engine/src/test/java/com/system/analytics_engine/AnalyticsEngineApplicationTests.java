09b5453fa4483970c6638044a016829cdf26239c
Hasan <hasan.mahdi7227@gmail.com>
2026-07-19 16:20:30 +0300

    chore: reorganize workspace into microservice monorepo layout

---
+++ analytics-engine/src/test/java/com/system/analytics_engine/AnalyticsEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.analytics_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
